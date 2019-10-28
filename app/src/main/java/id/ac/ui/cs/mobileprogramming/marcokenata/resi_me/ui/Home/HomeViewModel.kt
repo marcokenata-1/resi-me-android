192f545abb499e6b63c8f7909c85bc0b6798cd04
Marco Kenata <marco.kenata@ui.ac.id>
2019-10-28 13:47:32 +0700

    [CHORES] adding jobs in fragments

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/marcokenata/resi_me/ui/Home/HomeViewModel.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/marcokenata/resi_me/ui/Home/HomeViewModel.kt
@@ -1,7 +1,20 @@
 package id.ac.ui.cs.mobileprogramming.marcokenata.resi_me.ui.Home
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlin.coroutines.CoroutineContext
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : ViewModel(), CoroutineScope {
     // TODO: Implement the ViewModel
+
+    override val coroutineContext: CoroutineContext
+        get() = SupervisorJob() + Dispatchers.Main
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineContext.cancel()
+    }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/marcokenata/resi_me/ui/SavedMenu/SavedMenuViewModel.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/marcokenata/resi_me/ui/SavedMenu/SavedMenuViewModel.kt
@@ -1,7 +1,20 @@
 package id.ac.ui.cs.mobileprogramming.marcokenata.resi_me.ui.SavedMenu
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlin.coroutines.CoroutineContext
 
-class SavedMenuViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SavedMenuViewModel : ViewModel(), CoroutineScope {
+
+
+    override val coroutineContext: CoroutineContext
+        get() = SupervisorJob() + Dispatchers.Main
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineContext.cancel()
+    }
 }
